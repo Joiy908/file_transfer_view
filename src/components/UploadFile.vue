76b55d52cf791c7f5195e7503e58fe8b4ad827e8
joiy908 <joiy9088@gmail.com>
2023-02-27 22:23:56 +0800

    add my-upload:allow upload multiple files asynchronously

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -2,106 +2,129 @@
   <div>
     <h1>Upload File</h1>
     <form ref="myForm">
-      <input ref="file" type="file"/><br/>
+      <input
+        ref="file"
+        type="file"
+        multiple="multiple"
+        @change="onChange"
+      /><br />
     </form>
-    <button type="button" @click="upload()">Upload</button>
-    <br/>
-    <div v-if="uploadStatus.isUploading">
-      <el-progress :percentage="uploadStatus.uploadProgress" :status='uploadStatus.barStatus'></el-progress>
-      Progress:{{ uploadStatus.uploadProgress }}%<br/>
-      Result: {{ uploadStatus.result }}
+    <button type="button" @click="uploadAll()">Upload</button>
+    <div v-for="f in uploadList" :key="f.uid">
+      file name: {{ f.name }}, status: {{ f.status }}, progress:
+      {{ f.percentage }}, res: {{ f.res }}
+      <button @click="remove(f)">delete</button>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from "axios"
 
 export default {
   name: "UploadFile",
-  props: ['uploadDir'],
+  props: ["uploadDir"],
   data() {
     return {
-      uploadStatus: {
-        isUploading: false,
-        uploadProgress: 0,
-        result: '',
-        barStatus: ''
-      }
+      uploadList: [],
+      tempIndex: 1,
     }
   },
   computed: {
     uploadUrl() {
       // pass path as url parameter
-      return 'upload?dirPath=' + this.uploadDir;
-    }
+      // return "upload?dirPath=./files"
+      return "upload?dirPath=" + this.uploadDir;
+    },
   },
   methods: {
-    upload() {
-      if (this.uploadStatus.isUploading) {
-        alert('some file is uploading, please try again later.')
-        return;
+    onChange() {
+      let files = this.$refs.file.files
+      for (let i = 0; i < files.length; i++) {
+        this.handleStart(files[i])
+        // console.log(files[i]);
       }
+      this.$refs.myForm.reset()
+    },
+    handleStart(rawFile) {
+      rawFile.uid = Date.now() + this.tempIndex++;
+      let file = {
+        status: "ready",
+        name: rawFile.name,
+        size: rawFile.size,
+        percentage: 0,
+        uid: rawFile.uid,
+        raw: rawFile,
+      };
+      this.uploadList.push(file);
+    },
+    uploadAll() {
+      let files = this.uploadList;
+      // console.log("uploadAll is called");
+
+      for (let i = 0; i < files.length; i++) {
+        if (files[i].status == "ready") this.post(files[i])
+      }
+    },
+    post(file) {
       // 1. prepare data
-      let fileToUpload = this.$refs.file.files[0];
-      if (fileToUpload === undefined) {
-        alert('please select file to upload.')
-        return;
+      if (!file) {
+        return
       }
-      const data = new FormData();
-      data.append('file', fileToUpload);
+      const data = new FormData()
+      data.append("file", file.raw)
 
       // 2. write onUploadProgress listener
       const config = {
         onUploadProgress: (progressEvent) => {
           // use arrowFuc to share the 'this' of Vue
-          let totalLength = progressEvent.lengthComputable ? progressEvent.total :
-              progressEvent.target.getResponseHeader('content-length') ||
-              progressEvent.target.getResponseHeader('x-decompressed-content-length');
+          let totalLength = progressEvent.lengthComputable
+            ? progressEvent.total
+            : progressEvent.target.getResponseHeader("content-length") ||
+              progressEvent.target.getResponseHeader(
+                "x-decompressed-content-length"
+              )
           // console.log("onUploadProgress", totalLength);
           if (totalLength !== null) {
-            this.updateProgress(Math.round((progressEvent.loaded * 100) / totalLength));
+            file.percentage = Math.round(
+              (progressEvent.loaded * 100) / totalLength
+            )
+            file.status = "uploading"
           }
-        }
-      };
+        },
+      }
 
       // 3. make request
-      this.uploadStatus.isUploading = true;
-
       axios.post(this.uploadUrl, data, config).then(
-          res => { // use of arrow function
+      // axios
+      //   .post("http://192.168.0.104:8080/upload?dirPath=./files", data, config)
+      //   .then(
+          (res) => {
+            // use of arrow function
             // will make arrowFun share the same this with parentFun
-            this.uploadStatus.result = res.data;
-            console.log(res);
+            // console.log(res);
+            file.res = res.data;
             // tell DownloadTable to refresh by change the date of pleaseRefresh in vm
-            this.$emit('doRefresh');
+            this.$emit("doRefresh");
             // mark progress bar status
-            this.updateProgress.barStatus = "success";
+            file.status = "success";
           },
-          err => {
-            this.uploadStatus.result = err.data;
+          (err) => {
             console.log(err);
             // mark progress bar status
-            this.updateProgress.barStatus = "warning";
-          });
+            file.status = "fail";
+            file.res = err;
+          }
+        )
     },
-    updateProgress(progress) {
-      if (progress == 100) {
-        if (!this.uploadStatus.isUploading) {
-          return;
-        }
-        this.uploadStatus.isUploading =  false;
-        alert("uploaded √!");
-        this.$refs.myForm.reset();
-        return;
-      }
-      this.uploadStatus.isUploading =  true;
-      this.uploadStatus.uploadProgress = progress;
-    }
-  }
+    remove(file) {
+      // console.log("remove is called")
+      // console.log(file.uid);
+      let fileList = this.uploadList;
+      fileList.splice(fileList.indexOf(file), 1)
+    },
+  },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
