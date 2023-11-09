e96e2d5e319e2bd76477a75c93f06e4c80aadf3a
joiy908 <joiy9088@gmail.com>
2023-11-09 17:42:12 +0800

    change socketio URL

--- src/components/MsgBox.vue
+++ src/components/MsgBox.vue
@@ -3,14 +3,12 @@
     <h1>Message Box</h1>
     <span>messages:</span>
     <button @click="getMsgs()">refresh</button>
-    <ul>
+    <ul id="messages">
       <li v-for="msg in messages" :key="msg">
-        <button @click="copy2clipBoard(msg)">
-          copy</button>&nbsp; {{ msg }}
+        <div v-html="msg"></div>
       </li>
     </ul>
-    <label>new message: </label><button @click="pasteFromClipboard()">
-      paste from clipboard</button><br/>
+    <label>new message: </label><br/>
     <textarea id="msgInput" ref="msgInput" rows="4" cols="50"></textarea><br/>
     <button @click="putMsg">submit</button>
   </div>
@@ -27,49 +25,28 @@ export default {
   },
   methods: {
     async getMsgs() {
+      /**get messages and replace '\n' to '<br>' */
       const messages = await this.$store.dispatch('getMsgs');
       if (messages !== null)
-        this.messages = messages;
+        this.messages = messages.map((s) => s.replace(/\n/g, '<br>'));
     },
     async putMsg() {
-      if(await this.$store.dispatch('putMsg', this.$refs.msgInput.value))
-        this.getMsgs();
-    },
-    // limitStrLen(str) {
-    //   let N = 30;
-    //   if (str.length > N)
-    //     return str.substring(0, N) + '...(copy to see more)';
-    //   else
-    //     return str;
-    // },
-    async copy2clipBoard(str) {
-      try {
-        const result = await navigator.permissions.query({name: "clipboard-write"});
-        if (result.state === "granted" || result.state === "prompt") {
-          navigator.clipboard.writeText(str);
-          console.log('text copied!');
-        }
-        else throw "permissions deny"
-      } catch (e) {
-        console.log('fail to copy @@@', e);
-        alert('fail to copy @@@ '+ e);
-      }
-    },
-    async pasteFromClipboard() {
-      try{
-        const text = await navigator.clipboard.readText()
-        this.$refs.msgInput.value = text;
-        console.log('Text pasted.');
-      } catch(e) {
-        console.log('Failed to read clipboard. Using execCommand instead.');
+      if(await this.$store.dispatch('putMsg', this.$refs.msgInput.value)){
+        await this.getMsgs();
+        this.$refs.msgInput.value = '';
         this.$refs.msgInput.focus();
-        const result = document.execCommand('paste')
-        console.log('document.execCommand result: ', result);
+        window.scrollTo(0, document.body.scrollHeight);
       }
-    }
+    },
   },
   mounted() {
     this.getMsgs();
   }
 }
 </script>
+
+<style scoped>
+  #messages { list-style-type: none; margin: 0; padding: 0; }
+  #messages > li { padding: 0.5rem 1rem; }
+  #messages > li:nth-child(odd) { background: #efefef; }
+</style>
--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 import store from './store'
 
-const socket = io('http://127.0.0.1:8080'); // Replace with your server URL
+const URL = undefined // undefined means get it by js 
+const socket = io(URL);
 
 socket.on('refresh', () => {
   console.log('refresh msg gotten!');
